a2617c998554953d8a480c53e16fd0fba5a78053
Matheus Sena <matheus_osena@hotmail.com>
2023-05-22 15:17:58 -0300

    Setting Permissions

--- posts/views.py
+++ posts/views.py
@@ -2,12 +2,15 @@ from rest_framework import generics
 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+     permission_classes = (IsAuthorOrReadOnly,)
+     queryset = Post.objects.all()
+     serializer_class = PostSerializer
